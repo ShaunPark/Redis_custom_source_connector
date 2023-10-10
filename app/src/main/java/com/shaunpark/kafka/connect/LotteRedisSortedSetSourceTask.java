a20474703ce133a46462caf7a5ea134bb7f9ab5d
Sangho Park <spark@confluent.io>
2023-10-10 14:41:58 +0900

    클라이언트 분리

---
+++ app/src/main/java/com/shaunpark/kafka/connect/LotteRedisSortedSetSourceTask.java
@@ -0,0 +1,112 @@
+package com.shaunpark.kafka.connect;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.connect.source.SourceTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.kafka.common.utils.SystemTime;
+import org.apache.kafka.common.utils.Time;
+
+import com.shaunpark.kafka.connect.redis.RedisClient;
+
+/**
+ * FileStreamSourceTask reads from stdin or a file.
+ */
+public class LotteRedisSortedSetSourceTask extends SourceTask {
+    private static final Logger log = LoggerFactory.getLogger(LotteRedisSortedSetSourceTask.class);
+    public static final String REDIS_KEY = "REDIS_KEY";
+    public  static final String POSITION_FIELD = "position";
+    private static final Schema VALUE_SCHEMA = Schema.STRING_SCHEMA;
+
+    private String topic;
+    private int batchSize;
+    private RedisClient client;
+    private String redisKey;
+    private int pollInterval;
+
+    private Long streamOffset;
+    private Time time;
+    
+    public LotteRedisSortedSetSourceTask() {
+    }
+
+    @Override
+    public String version() {
+        return new LotteRedisSortedSetSourceConnector().version();
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+        AbstractConfig config = new AbstractConfig(LotteRedisSortedSetSourceConnector.CONFIG_DEF, props);
+        String hostName = config.getString(LotteRedisSortedSetSourceConnector.REDIS_HOST_CONFIG);
+        int portNumber = config.getInt(LotteRedisSortedSetSourceConnector.REDIS_PORT_CONFIG);
+        pollInterval = config.getInt(LotteRedisSortedSetSourceConnector.POLL_MAX_INTERVAL_MS_CONFIG);
+
+        redisKey = config.getString(LotteRedisSortedSetSourceConnector.REDIS_KEY_CONFIG);
+        topic = config.getString(LotteRedisSortedSetSourceConnector.TOPIC_CONFIG);
+        batchSize = config.getInt(LotteRedisSortedSetSourceConnector.TASK_BATCH_SIZE_CONFIG);
+        time = new SystemTime();
+
+        client = new RedisClient(hostName, portNumber, batchSize, redisKey);
+        client.connect();
+    }
+
+
+    // sorted set에서 데이터 조회
+    // 
+    @Override
+    public List<SourceRecord> poll() throws InterruptedException {
+        streamOffset = System.currentTimeMillis();
+        log.info("Poll started : " + streamOffset);
+        List<String>data = client.getSendData(streamOffset);
+
+        List<SourceRecord> records = new ArrayList<SourceRecord>();
+        if( data.size() == 0 ) {
+            time.sleep(pollInterval);
+        } else {
+            for ( String message : data) {
+                records.add(new SourceRecord(offsetKey(redisKey), offsetValue(streamOffset), topic, null,
+                null, null, VALUE_SCHEMA, message, System.currentTimeMillis()));
+            }
+        }
+
+        return records;
+    }
+
+    // delete sent records from REDIS
+    public void commitRecord(SourceRecord record, RecordMetadata metadata) throws InterruptedException {
+        try {
+            client.delete((String)record.value());
+        } catch(Exception e) {
+            log.error("error in commitRecord", e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        log.trace("Stopping");
+        synchronized (this) {
+            if (client != null ) {
+                client.close();
+                log.trace("Closed redis connection");
+            }
+            this.notify();
+        }
+    }
+
+    private Map<String, String> offsetKey(String key) {
+        return Collections.singletonMap(REDIS_KEY, key);
+    }
+
+    private Map<String, Long> offsetValue(Long pos) {
+        return Collections.singletonMap(POSITION_FIELD, pos);
+    }
+}
---
+++ app/src/main/java/com/shaunpark/kafka/connect/redis/RedisClient.java
@@ -0,0 +1,112 @@
+package com.shaunpark.kafka.connect.redis;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
+public class RedisClient {
+    private String key;
+    private String host;
+    private int port;
+    private JedisPool pool;
+    private int batchSize;
+
+    private static final Logger log = LoggerFactory.getLogger(RedisClient.class);
+    private final static Hashtable<String,Long> producedMessages = new Hashtable<String,Long>();
+    private static long cleanTime = 0;
+
+    public RedisClient(String hostName, int port, int batchSize, String key) {
+        this.host = hostName;
+        this.port = port;
+        this.key = key;
+        this.batchSize = batchSize;
+    }
+
+    private int retryCount = 10;
+    public void connect() {
+        if(pool == null || pool.isClosed()) {
+            while(true) {
+                try {
+                    connect();
+                    retryCount = 10;
+                    break;
+                } catch( JedisConnectionException je) {
+                    if ( retryCount > 0 ) {
+                        retryCount--;
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            break;
+                        }
+                    } else {
+                        throw je;
+                    }
+                }
+            } 
+            retryCount = 10;
+        }
+    }
+
+    private static void clean() {
+        long nowMs = System.currentTimeMillis();
+        if (cleanTime == 0 ) {
+            cleanTime = nowMs;
+        }
+        if( nowMs - cleanTime > 600000  ) { // 10 mins
+            Enumeration<String> keys =  producedMessages.keys();
+            while(keys.hasMoreElements()) {
+                String key = keys.nextElement();
+                long ms = producedMessages.get(key);
+                if ( nowMs - ms > 3600000 ) {
+                    producedMessages.remove(key);
+                }
+            }
+        }
+    }
+    
+    public List<String> getSendData(long endPosition) {
+        connect();
+        long pollTime = System.currentTimeMillis();
+        ArrayList<String> newData = new ArrayList<String>();
+        try (Jedis jedis = pool.getResource()) {
+            List<String> list = jedis.zrangeByScore(key, 0, endPosition, 0, batchSize);
+            for( String msg : list) {
+                if( producedMessages.contains(msg)) {
+                    continue;
+                } else {
+                    newData.add(msg);
+                    producedMessages.put(msg, pollTime);
+                }
+            }
+            return newData;
+        } finally {
+            clean();
+        }
+    }
+
+    public void delete(String deleteItems) {
+        connect();
+
+        try (Jedis jedis = pool.getResource()) {
+
+            if( producedMessages.contains(deleteItems)) {
+                producedMessages.remove(deleteItems);
+                log.info("commitRecord : delete processed record" + deleteItems);
+                jedis.zrem(key, deleteItems);
+            }
+        }
+    }
+
+    public void close() {
+        if(pool != null && !pool.isClosed())
+            pool.close();
+    }
+}
